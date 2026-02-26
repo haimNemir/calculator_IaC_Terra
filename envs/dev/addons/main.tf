41efaf56bbc9b291872dd378c0049c6f3c5bf85f
Haim Nemirovsky <chimnem@gmail.com>
2026-02-26 17:32:45 +0200

    add some notes on some blocks

--- envs/dev/addons/main.tf
+++ envs/dev/addons/main.tf
@@ -17,18 +17,18 @@ terraform {
   required_version = ">= 1.5.0" # required_version of terraform.
   
   # required_providers - Ask from terraform to download the providers.
-  required_providers { # Here we are defining the providers that we will use in this module. Only with those providers we can define resources such as aws_eks_cluster, kubernetes_namespace, because Terraform doesn't know how to speak to AWS or Kubernetes without the providers. And those providers convert the Terraform code requests into API of those platforms as a regular requests.
-    aws = {
+  required_providers { # Here we are defining the providers that we will use in this module.    Only with those providers we can define resources such as aws_eks_cluster, kubernetes_namespace, because Terraform doesn't know how to speak to AWS or Kubernetes without the providers. And those providers convert the Terraform code requests into API of those platforms as a regular requests.
+    aws = {         # We use AWS provider to create resources on AWS, such as EKS cluster, VPC, etc.
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
 
-    kubernetes = {
+    kubernetes = {  # We use Kubernetes provider to create resources on the cluster (AWS provider can't do that), such as namespaces, deployments, etc. And the main porpuse of this provider is to deploy specific resources on the cluster.
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
 
-    helm = {
+    helm = {        # We use Helm provider to deploy Helm charts on the cluster, such as AWS Load Balancer Controller, ArgoCD, Prometheus, ExternalDNS. And the kubernetes provider can't do that, because it's only for deploying specific resources, and Helm charts are a collection of resources that are defined in a specific way, and the Helm provider knows how to deploy them on the cluster.
       source  = "hashicorp/helm"
       version = "~> 2.0"
     }
@@ -44,15 +44,15 @@ provider "aws" {
   region = data.terraform_remote_state.foundation.outputs.region 
 }
 
-# Here we are defining the kubernetes provider, which is required to access the cluster and deploy resources on it. We need him in addition to the AWS provider, because AWS provider is only for creating resources on AWS, 
+# Here we are defining the kubernetes provider, which is required to access the cluster and deploy resources on it. We need him in addition to the AWS provider, because AWS provider is only for creating resources on AWS.  
 provider "kubernetes" { # Here we define how to access from our local kubernetes provider to the API Server of the cluster.
   host                   = data.terraform_remote_state.foundation.outputs.eks_cluster_endpoint  # Here we define the host of the API Server of the cluster, which is required to access to the correct cluster.
   cluster_ca_certificate = base64decode(data.terraform_remote_state.foundation.outputs.cluster_ca) # CA is a certificate authority that proves the identity of the cluster, without it we can't be sure that the target we are trying to access is the correct target, it's always can be "Man in the middle" attack. And with this CA we can be sure that we are accessing the correct target (the cluster), and not some fake cluster that someone created to steal our credentials. We need to decode it because it's encoded in base64 format, and we need to decode it to get the actual certificate.
   token                  = data.aws_eks_cluster_auth.this.token # We are using the token that we got from AWS above (data "aws_eks_cluster_auth" "this") to access to the cluster.
 }
 
-provider "helm" {
-  kubernetes {
+provider "helm" { 
+  kubernetes { # With this Helm provider we are defining on the cluster of kubernetes some charts, and to do that we need to get access to the cluster, and that's is what we are doing here - we provide the certificate of access to the cluster to the helm provider, as we did with the kubernetes provider above.
     host                   = data.terraform_remote_state.foundation.outputs.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.foundation.outputs.cluster_ca)
     token                  = data.aws_eks_cluster_auth.this.token # We are using the token that we got from AWS above (data "aws_eks_cluster_auth" "this") to access to the cluster
