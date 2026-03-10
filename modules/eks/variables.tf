efd386b94a60027f2cbcf45e28e2cd39f6ac16cb
Haim Nemirovsky <chimnem@gmail.com>
2026-03-10 16:54:06 +0200

    adding explaination to the addons - NO CODE CHANGED

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -82,15 +82,16 @@ variable "addons" {
   # Syntax of object is: "object({name = string \n age = number})" and you must enter inside of new each instance those variables from those types.
   # Syntax of optional is:  ... = optional(type, default value). And this function (optional) is basically makes this variable inside the object net necessary so if you will not define his value - he will make this value as "null", And if there is a default value to this variable then the value will be the default value and when you will explicitly define his value it must to be matched with the value type of this variable. 
   # Here when we do this: map(object{...})   -  this mean that we create here a teplate for variable from type of map (contain key/value pair), and his key/value will be the "object name" (key) + the "object values" (what inside the kurly brackets). So when we want to inintial instance from this variable like inside env/dev/main/ inside block of module "eks" - we will need to to this in this syntax, so it will look like this: addon (key of map) = {...}(what inside the curly brackets is the value of map). 
-  type = map(object({
-    name                        = optional(string) # optional: is a variable with the format of (type, default)
-    before_compute              = optional(bool, false)
-    most_recent                 = optional(bool, true)
-    addon_version               = optional(string)
-    configuration_values        = optional(string)
-    preserve                    = optional(bool, true)
-    resolve_conflicts_on_create = optional(string, "NONE")
-    resolve_conflicts_on_update = optional(string, "OVERWRITE")
+  type = map(object({ # In two words - we create here a template like "Class" in java, and in the modules we will create "objects" from this "Class".
+    # optional: is a variable with the format of (type, default)
+    name                        = optional(string)                # Name of the addon.
+    before_compute              = optional(bool, false)           # If true, the addon will be created before the creation of the nodes.
+    most_recent                 = optional(bool, true)            # If true, the most recent version of the addon will be used.
+    addon_version               = optional(string)                # Version of the addon to use. If not specified, the most recent version will be used.
+    configuration_values        = optional(string)                # A JSON string that contains a specific configuration values for the addon.
+    preserve                    = optional(bool, true)            # If true, the addon will not be deleted when the cluster is deleted or in other scenarios like this.
+    resolve_conflicts_on_create = optional(string, "NONE")        # Determines how to behave when there is a conflict during addon creation. For example, if an addon with the same name already exists - what to do? "OVERRIDE" - means to override the existing addon, "NONE" - means to do nothing and keep the existing and stop "terraform apply" , "DELETE" - means to delete the existing addon and create a new one. 
+    resolve_conflicts_on_update = optional(string, "OVERWRITE")   # If we decided to update the addon to a new version.
   }))
   default = null
 }
